165d6edf7d362bc2bfa8984c678fc0bdd95af072
yonraz <yraz530@gmail.com>
2024-05-02 17:42:23 +0300

    set up sudoku service, client request for image upload

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BoardComponent } from '../board/board.component';
+import { LandingComponent } from '../landing/landing.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BoardComponent],
+  imports: [CommonModule, RouterOutlet, BoardComponent, LandingComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
